67e9229f92f63fe835bf570c38020e6bfc16e5f6
Gamattowicz <p.romanczuk31@gmail.com>
2021-02-06 22:06:35 +0100

    add upload images function

--- articles/models.py
+++ articles/models.py
@@ -5,7 +5,9 @@ class Article(models.Model):
     title = models.CharField(max_length = 150, verbose_name = 'Tytuł')
     content = models.TextField(verbose_name = 'Zawartość')
     published = models.DateTimeField(verbose_name = 'Data publikacji')
-    # image = models.FileField(upload_to='images', verbose_name='Picture')
+    image = models.FileField(upload_to='images', verbose_name='Picture', default='test')
+    def __str__(self): 
+        return self.title 
     
     def __unicode__(self):
         return self.title 
