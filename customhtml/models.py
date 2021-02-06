564b29c7e605600b944068fe50adcc80c9b48bda
Gamattowicz <p.romanczuk31@gmail.com>
2021-02-06 22:45:16 +0100

    add render_html app

---
+++ customhtml/models.py
@@ -0,0 +1,10 @@
+#-*- coding: utf-8 -8-
+from django.db import models
+
+
+class CustomHtml(models.Model):
+    title = models.CharField(max_length = 150, verbose_name = 'Title')
+    sourcehtml = models.TextField(verbose_name = 'HTML code')
+    
+    def __unicode__(self):
+        return self.title
