e5902daadf22ce4945b09944faaa498a924c6ee6
Gamattowicz <p.romanczuk31@gmail.com>
2021-02-07 16:30:33 +0100

    add comment system

--- articles/models.py
+++ articles/models.py
@@ -1,13 +1,21 @@
-#-*- coding: utf-8 -*-
 from django.db import models
 
+
 class Article(models.Model):
-    title = models.CharField(max_length = 150, verbose_name = 'Tytuł')
-    content = models.TextField(verbose_name = 'Zawartość')
-    published = models.DateTimeField(verbose_name = 'Data publikacji')
-    image = models.FileField(upload_to='images', verbose_name='Picture', default='test')
-    def __str__(self): 
-        return self.title 
+    title = models.CharField(max_length=150, verbose_name = 'Tytuł')
+    content = models.TextField(verbose_name= 'Zawartość')
+    published = models.DateTimeField(verbose_name= 'Data publikacji')
+    image = models.FileField(upload_to='images', verbose_name='Obrazek')
     
     def __unicode__(self):
-        return self.title 
+        return self.title
+
+
+class Comment(models.Model):
+    name = models.CharField(max_length=150, verbose_name = 'Użytkownik')
+    content = models.TextField(verbose_name= 'Komentarz')
+    published = models.DateTimeField(verbose_name= 'Data publikacji')
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    
+    def __unicode__(self):
+        return self.name
--- articles/views.py
+++ articles/views.py
@@ -1,8 +1,36 @@
 from django.shortcuts import render
-from articles.models import Article
+from articles.models import Article, Comment
+from articles.forms import CommentForm
+from django.template.context_processors import csrf
+from django.utils import timezone
+from django.http import HttpResponseRedirect
+
 
 def articles(request):
-    return render(request, 'articles.html', {'articles' : Article.objects.all()})
+    return render(request, 'articles.html', {'articles' : Article.objects.all()}) 
 
 def article(request, article_id):
-    return render(request, 'article.html', {'article': Article.objects.get(id = article_id)})
+    return render(request, 'article.html', {'article' : Article.objects.get(id=article_id)})
+
+def add_comment(request,article_id):
+	art = Article.objects.get(id=article_id)
+
+	if request.method == "POST":
+		cf = CommentForm(request.POST)
+		if cf.is_valid():
+			comment = cf.save(commit=False)
+			comment.published = timezone.now()
+			comment.article = art
+			comment.save()
+
+			return HttpResponseRedirect('/article/%s' % article_id)
+
+	else:
+		cf = CommentForm()
+
+	args = {}
+	args.update(csrf(request))
+	args['article'] = art
+	args['form'] = cf
+
+	return render(request, 'add_comment.html', args)
